ca82926a94f4ad2a8eeb57d48621f2ce33e4a2f3
Denos Kume <110854822+denoskume@users.noreply.github.com>
2024-12-06 19:10:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -211,8 +211,15 @@ function toggleUnderlineLinks() {
 
 function highlightLinks() {
     const links = document.querySelectorAll('a');
-    links.forEach(link => link.style.backgroundColor = 'yellow');
+    links.forEach(link => {
+        if (link.style.backgroundColor === 'yellow') {
+            link.style.backgroundColor = '';
+        } else {
+            link.style.backgroundColor = 'yellow';
+        }
+    });
 }
+
 
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
