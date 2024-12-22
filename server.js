de43fc169a5e21076a8a69b3b7aa84091ab57c6c
Denos Kume <denoskume77@gmail.com>
2024-12-22 11:44:07 +0530

    Organize project files into separate folders

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const path = require('path'); // Import path module
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define route for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Adjust the path to your main HTML file
+});
 
 app.post('/send-email', (req, res) => {
   const { name, email, subject, message } = req.body;
